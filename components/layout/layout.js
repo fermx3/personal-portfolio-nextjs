43ccfe7176a1579d937beb4bf2d7a96cb030186d
fermx3 <99512170+fermx3@users.noreply.github.com>
2024-07-24 18:25:47 -0600

    Update layout.js

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -11,7 +11,7 @@ const montserrat = Montserrat({ subsets: ['latin'] });
 const Layout = ({ children }) => {
   return (
     <Fragment>
-      <MainNavigation font={montserrat.className} />
+    {/* <MainNavigation font={montserrat.className} /> */}
       <main className={`${montserrat.className} ${classes.main}`}>
         {children}
       </main>
